f8ce5cf9e339144f3ae2c2b3609449cf515d526d
Maisha Rahman Mim <52733601+maisharahman876@users.noreply.github.com>
2022-12-15 04:28:23 +0600

    offline1 task1 kichuta

---
+++ Offline2/data_handler.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+
+def load_dataset(filename):
+    """
+    function for reading data from csv
+    and processing to return a 2D feature matrix and a vector of class
+    :return:
+    """
+    # todo: implement
+    # read data from csv
+    df=pd.read_csv(filename)
+    features=list(df.columns[:-1])
+    X=df[features]
+    y=df['isoriginal']
+
+    # return 2D feature matrix and a vector of class
+    return X,y
+
+
+def split_dataset(X, y, test_size, shuffle):
+    """
+    function for spliting dataset into train and test
+    :param X:
+    :param y:
+    :param float test_size: the proportion of the dataset to include in the test split
+    :param bool shuffle: whether to shuffle the data before splitting
+    :return:
+    """
+    # todo: implement.
+    train_size = int(len(X) * (1 - test_size))
+    X_new=X
+    X_new['Class']=y
+    if shuffle:
+        #join X and y
+        X_new=X_new.sample(frac=1)
+    features=list(X_new.columns[:-1])
+    X_train=X_new[:train_size]
+    y_train=X_train['Class'].to_numpy()
+    X_train=X_train[features].to_numpy()
+    X_test=X_new[train_size:]
+    y_test=X_test['Class'].to_numpy()
+    X_test=X_test[features].to_numpy()
+    return X_train, y_train, X_test, y_test
+
+
+def bagging_sampler(X, y):
+    """
+    Randomly sample with replacement
+    Size of sample will be same as input data
+    :param X:
+    :param y:
+    :return:
+    """
+    # todo: implement
+    X_sample, y_sample = None, None
+    assert X_sample.shape == X.shape
+    assert y_sample.shape == y.shape
+    return X_sample, y_sample
---
+++ Offline2/linear_model.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+class LogisticRegression:
+    def __init__(self, params):
+        """
+        figure out necessary params to take as input
+        :param params:
+        """
+        # todo: implement
+        self.learning_rate=params['learning_rate']
+        self.iteration=params['iteration']
+    def sigmoid(self,X,theta,bias):
+        z=np.dot(X,theta)+bias
+        return 1/(1+np.exp(-z))
+    def fit(self, X, y):
+        """
+        :param X:
+        :param y:
+        :return: self
+        """
+        assert X.shape[0] == y.shape[0]
+        assert len(X.shape) == 2
+        # todo: implement
+        self.X=X
+        self.y=y
+        self.rows,self.cols=X.shape
+        self.theta=np.zeros(self.cols)
+        self.bias=0
+        theta1=self.theta
+        min_cost=99999999
+
+        #gradient descent
+        for i in range(self.iteration):
+            y_pred=self.sigmoid(self.X,self.theta,self.bias)
+            dw=(1/self.rows)*np.dot(self.X.T,(y_pred-self.y))
+            db=(1/self.rows)*np.sum(y_pred-self.y)
+            self.theta-=self.learning_rate*dw
+            self.bias-=self.learning_rate*db
+            #cost function
+            cost=-(1/self.rows)*np.sum(self.y*np.log(y_pred)+(1-self.y)*np.log(1-y_pred))
+            #taking track of minimum cost
+            if cost>min_cost:
+                min_cost=cost
+                theta1=self.theta
+
+        self.theta=theta1
+
+
+
+    def predict(self, X):
+        """
+        function for predicting labels of for all datapoint in X
+        :param X:
+        :return:
+        """
+        # todo: implement
+        y_pred=self.sigmoid(X,self.theta,self.bias)
+        y_pred=np.where(y_pred>0.5,1,0)
+        return y_pred
